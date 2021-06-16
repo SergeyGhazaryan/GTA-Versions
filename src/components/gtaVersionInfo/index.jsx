b3cc0bd593ea08b9dbcdc6705448104cd5daffc9
SergeyGhazaryan <sergey.ghazaryan2000@gmail.com>
2021-06-16 11:50:16 +0400

    Minor change

--- src/components/gtaVersionInfo/index.jsx
+++ src/components/gtaVersionInfo/index.jsx
@@ -1,14 +1,15 @@
+import { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import { data } from '../../data';
 
 const GtaVersionInfo = () => {
   const { id } = useParams();
 
-  const information = () => {
-    const selectedVersion = data.find((d) => d.id == id);
+  const information = useMemo(() => {
+    const selectedVersion = data.find((d) => d.id.toString() === id);
     if (!selectedVersion) return;
     return selectedVersion.information;
-  };
+  }, [id]);
 
   return <div>{information}</div>;
 };
